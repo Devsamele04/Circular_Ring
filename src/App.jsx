a21ce9a48c73acc956ad88e2f0f7b5d60089fe8d
Dev samele <devsamele385@gmail.com>
2025-03-24 23:00:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./style.css";
+import { OrbitControls } from "@react-three/drei";
+import Cyl from "./cyl";
+import {
+  Bloom,
+  EffectComposer,
+  ToneMapping,
+} from "@react-three/postprocessing";
+
+const App = () => {
+  return (
+    <>
+      <div className="absolute uppercase left-[37%] top-[3%]">
+        <h1 className=" text-white text-4xl text-center">
+          Welcome to my Page.
+        </h1>
+      </div>
+      <Canvas flat camera={{ fov: 30 }}>
+        <ambientLight />
+        <Cyl />
+        <EffectComposer>
+          <Bloom
+            mipmapBlur
+            intensity={12.0} // The bloom intensity.
+            luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+          />
+        </EffectComposer>
+      </Canvas>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Cyl.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import { useTexture } from "@react-three/drei";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+
+const Cyl = () => {
+  let tex = useTexture("/adv.jpg");
+  let Cyl = useRef(null);
+  useFrame((state, deleta) => {
+    Cyl.current.rotation.y += deleta;
+  });
+  return (
+    <group rotation={[0, 1.4, 0.5]}>
+      <mesh ref={Cyl}>
+        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+        <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+      </mesh>
+    </group>
+  );
+};
+
+export default Cyl;
