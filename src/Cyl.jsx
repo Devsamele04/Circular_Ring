94944536e2a17e27a084a0cd0484e61dbc2b5dfe
Dev samele <devsamele385@gmail.com>
2025-04-10 18:49:41 +0530

    add pic

--- src/Cyl.jsx
+++ src/Cyl.jsx
@@ -14,7 +14,7 @@ const Cyl = () => {
   return (
     <group rotation={[0, 1.4, 0.5]}>
       <mesh ref={Cyl}>
-        <cylinderGeometry args={[1, 1, 1, 60, 60, true]} />
+        <cylinderGeometry args={[1, 1, 1, 40, 40, true]} />
         <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
       </mesh>
     </group>
